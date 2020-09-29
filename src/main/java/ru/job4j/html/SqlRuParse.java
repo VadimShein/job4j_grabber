eaae0e1486e622b807c2f32ccf584965f15b3640
VadimShein <shein.v94@mail.ru>
2020-09-29 23:22:38 +0600

    2.1. Преобразование даты [#303908]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,17 +5,51 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements row = doc.select(".postslisttopic");
+
+        DateFormatSymbols myDateFormatSymbols = new DateFormatSymbols(){
+            @Override
+            public String[] getMonths() {
+                return new String[] {"янв", "февр", "марта", "апр", "мая", "июня",
+                        "июля", "авг", "сен", "окт", "ноя", "дек"};
+            }
+        };
 
         for (Element td : row) {
             Element href = td.child(0);
             Element hrefDate = td.lastElementSibling();
             System.out.println(href.attr("href"));
             System.out.println(href.text());
-            System.out.println(hrefDate.text() + System.lineSeparator());
+            String hrefDateTxT = hrefDate.text();
+
+            if (hrefDateTxT.contains("сегодня")) {
+                SimpleDateFormat sdf = new SimpleDateFormat("d M yyyy H:m");
+                final Calendar cal = Calendar.getInstance();
+                System.out.println(sdf.format(cal.getTime()));
+
+            }
+            else if (hrefDateTxT.contains("вчера")) {
+                SimpleDateFormat sdf = new SimpleDateFormat("d M yyyy H:m");
+                final Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.DATE, -1);
+                System.out.println(sdf.format(cal.getTime()));
+            } else {
+                Date date = new SimpleDateFormat("d MMM yy, H:m", myDateFormatSymbols).parse(hrefDateTxT);
+                Locale locales = Locale.getDefault();   //ru_RU
+                String patterns = "d M yyyy H:m";
+                String modData = new SimpleDateFormat(patterns, locales).format(date);
+                System.out.println(modData);
+            }
         }
     }
 }
