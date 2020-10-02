7b31fccfc0f96e62ad9ff2ca044527a0774c5569
VadimShein <shein.v94@mail.ru>
2020-10-02 21:01:16 +0600

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#303904]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,57 +5,69 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.text.DateFormatSymbols;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
+import java.util.*;
 
 
 public class SqlRuParse {
-    public static void main(String[] args) throws Exception {
-        DateFormatSymbols myDateFormatSymbols = new DateFormatSymbols(){
-            @Override
-            public String[] getMonths() {
-                return new String[] {"янв", "фев", "мар", "апр", "мая", "июн",
-                        "июл", "авг", "сен", "окт", "ноя", "дек"};
-            }
-        };
+    public void parseList(String url, int pages) throws IOException, ParseException {
+        String postUrl ;
+        String postName;
+        String postDate;
 
-        int ind = 0;
-        while (ind < 5) {
-            String url = "https://www.sql.ru/forum/job-offers/" + (ind + 1);
-            Document doc = Jsoup.connect(url).get();
+        int index = 0;
+        while (index < pages) {
+            String PageUrl = url + (index + 1); //"https://www.sql.ru/forum/job-offers/"
+            Document doc = Jsoup.connect(PageUrl).get();
             Elements row = doc.select(".postslisttopic");
-
-            System.out.println("page " + (ind + 1) + System.lineSeparator());
+            System.out.println(System.lineSeparator() + "Page: " + (index + 1) + System.lineSeparator());
             for (Element td : row) {
                 Element href = td.child(0);
                 Element hrefDate = td.lastElementSibling();
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                String hrefDateTxT = hrefDate.text();
+                postUrl = href.attr("href");
+                postName = href.text();
+                postDate = SqlRuParse.convertDate(hrefDate.text());
 
-                if (hrefDateTxT.contains("сегодня")) {
-                    SimpleDateFormat sdf = new SimpleDateFormat("d M yyyy H:m");
-                    final Calendar cal = Calendar.getInstance();
-                    System.out.println(sdf.format(cal.getTime()));
-
-                }
-                else if (hrefDateTxT.contains("вчера")) {
-                    SimpleDateFormat sdf = new SimpleDateFormat("d M yyyy H:m");
-                    final Calendar cal = Calendar.getInstance();
-                    cal.add(Calendar.DATE, -1);
-                    System.out.println(sdf.format(cal.getTime()));
-                } else {
-                    Date date = new SimpleDateFormat("d MMM yy, H:m", myDateFormatSymbols).parse(hrefDateTxT);
-                    Locale locales = Locale.getDefault();   //ru_RU
-                    String patterns = "d M yyyy H:m";
-                    String modData = new SimpleDateFormat(patterns, locales).format(date);
-                    System.out.println(modData);
-                }
+                System.out.println(postUrl);
+                System.out.println(postName);
+                System.out.println(postDate);
             }
-            ind++;
+            index++;
         }
+    }
+
+    public static String convertDate(String postDate) throws ParseException {
+        String modData;
+        DateFormatSymbols myDateFormatSymbols = new DateFormatSymbols(){
+            @Override
+            public String[] getMonths() {
+                return new String[] {"янв", "фев", "мар", "апр", "май", "июн",
+                        "июл", "авг", "сен", "окт", "ноя", "дек"};
+            }
+        };
+        if (postDate.contains("сегодня")) {
+            SimpleDateFormat sdf = new SimpleDateFormat("d M yyyy H:m");
+            final Calendar cal = Calendar.getInstance();
+            modData = sdf.format(cal.getTime());
+        } else if (postDate.contains("вчера")) {
+            SimpleDateFormat sdf = new SimpleDateFormat("d M yyyy H:m");
+            final Calendar cal = Calendar.getInstance();
+            cal.add(Calendar.DATE, -1);
+            modData = sdf.format(cal.getTime());
+        } else {
+            Date date = new SimpleDateFormat("d MMM yy, H:m", myDateFormatSymbols).parse(postDate);
+            Locale locales = Locale.getDefault();   //ru_RU
+            String patterns = "d M yyyy H:m";
+            modData = new SimpleDateFormat(patterns, locales).format(date);
+        }
+        return modData;
+    }
+
+    public static void main(String[] args) throws Exception {
+        SqlRuParse sqlParse = new SqlRuParse();
+        sqlParse.parseList("https://www.sql.ru/forum/job-offers/", 5);
     }
 }
